acffa0e5c506404dff4003557910a8642707c0de
Wander Aquino <wander.aquino@gmail.com>
2021-03-26 01:19:10 -0300

    Incluindo arquivo para tratamento do include do Header na página

---
+++ src/js/core/includes.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+
+function loadIncludes(parent) {
+    if(!parent) {
+        parent = "body";
+    }
+    $(parent).find("[wm-include]").each(function (index,element) {
+        const url = $(element).attr("wm-include");
+        $.ajax({
+            url,
+            success (data) {
+                $(element).html(data);
+                $(element).removeAttr("wm-include");
+                loadIncludes(element);
+            }
+        })
+    });
+}
+
+loadIncludes();
